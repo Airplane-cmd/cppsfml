0deb51bda05647cd417d7896aa9b674fdea408bf
Airplane-cmd <simonpatrikoff1@outlook.com>
2022-02-27 20:49:32 +1000

    minor2

--- src/atome/src/sfml0.cpp
+++ src/atome/src/sfml0.cpp
@@ -8,11 +8,110 @@
 	int xPole = 1920;
 	int yPole = 1080;
 	sf::RenderWindow window(sf::VideoMode(xPole, yPole), "std::string *Freudistic_name");
+
+//class Atome;
+class Extremum
+{
+	private:
+		double m_max;
+		double m_min;
+	public:
+		Extremum(double max, double min) : m_max{max}, m_min{min}{};
+		double getMax()
+		{
+			return m_max;
+		}
+		double getMin()
+		{
+			return m_min;
+		}
+};
+
+class Vector
+{
+        private:
+                double m_x;
+                double m_y;
+        public:
+                Vector(double x = 0, double y = 0)
+                {
+                        m_x = x;
+                        m_y = y;
+                };
+                Vector(int x, int y)
+                {
+                        m_x = x;
+                        m_y = y;
+                };
+
+                //Vector(double x, double y) 
+                friend Vector operator+(const Vector &v1, const Vector &v2);
+                friend Vector operator-(const Vector &v1, const Vector &v2);
+
+                void operator=(const Vector &v1)
+                {
+                        m_x = v1.m_x;
+                        m_y = v1.m_y;
+                }
+                double getVectorValue()
+                {        
+                        return sqrt(m_x*m_x+m_y*m_y);
+                }
+                void setVector(double x, double y)
+                {
+                        m_x = x;
+                        m_y = y;
+                }
+                
+};
+Vector operator+(const Vector &v1, const Vector &v2)
+{
+        return Vector(v1.m_x + v2.m_x, v1.m_y + v2.m_y);
+}
+Vector operator-(const Vector &v1, const Vector &v2)
+{
+        return Vector(v1.m_x - v2.m_x, v1.m_y - v2.m_y);
+}
+
+Vector setVector(int xa, int ya)
+{
+        return Vector(xa, ya);
+}
+
+class Atome
+{
+        private:
+                double m_charge;
+                int m_rad;
+                int m_mass;
+                bool m_static;
+                Vector m_pos;
+                Vector m_velosity;
+                Vector m_accel;
+                Vector m_sumForce;
+                Vector m_elS;
+                sf::Color color;
+		
+        public:
+		friend class Continuum;
+		friend class Vector;
+                Atome(Vector vec = setVector(0, 0), double charge = 1, int rad = 50, int mass = 1, bool staticP = 0) : m_charge{charge}, m_rad{rad}, m_mass{mass}, m_static{staticP}, m_pos{vec}
+		{
+
+		}
+                void displayAtom(Atome ex)
+                {
+
+                        sf::CircleShape atom(m_rad);
+                }
+    
+};
+
 class Continuum
 {
 	private:
 		int m_atomes = 0;
-		Atome* m_atomes = new Atomes[m_atomes];
+		Atome* m_atomesA = new Atome[m_atomes];
 		double* m_acceles = new double[m_atomes];
 		double* m_velocities = new double[m_atomes];
 		double** m_S = new double*[m_atomes];
@@ -24,9 +123,30 @@ class Continuum
                         	m_S[m_atomes] = new double[m_atomes];
 
 		}
-		void addAtoms()
+		friend class Atome;
+		/*void discon()//every iterative functuon of comtinuum
 		{
-			++m_atomes;
+			selectColour();	
+		}*/
+		void addAtoms(Atome atome)
+                {
+                        ++m_atomes;
+                        //m_atomesA[m_atomes] = &atome;
+
+                }
+		Extremum setColour()
+		{
+			Vector max = setVector(0, 0);
+			Vector min = setVector(0, 0);
+			for(int i = 0; i < m_atomes; ++i)
+			{
+				if (m_atomesA[i].m_velosity.getVectorValue()>max.getVectorValue())
+					max = m_atomesA[i].m_velosity;
+				else if(m_atomesA[i].m_velosity.getVectorValue()<min.getVectorValue())
+					min = m_atomesA[i].m_velosity;
+			}
+			return Extremum(max.getVectorValue(),min.getVectorValue());
+
 
 		}
 		void wait60()
@@ -45,85 +165,6 @@ class Continuum
 		}
 
 };
-class Vector
-{
-	private:
-		double m_x;
-		double m_y;
-	public:
-		Vector(double x = 0, double y = 0)
-		{
-			m_x = x;
-			m_y = y;
-		};
-		Vector(int x, int y)
-                {
-                        m_x = x;
-                        m_y = y;
-                };
-
-		//Vector(double x, double y) 
-		friend Vector operator+(const Vector &v1, const Vector &v2);
-		void operator=(const Vector &v1)
-		{
-			m_x = v1.m_x;
-			m_y = v1.m_y;
-		}
-		double getVectorVelue()
-                {        
-                        return sqrt(m_x*m_x+m_y*m_y);
-                }
-		void setVector(double x, double y)
-		{
-			m_x = x;
-			m_y = y;
-		}
-		
-};
-Vector operator+(const Vector &v1, const Vector &v2)
-{
-	return Vector(v1.m_x + v2.m_x, v1.m_y + v2.m_y);
-}
-Vector setVector(int xa, int ya)
-{
-        return Vector(xa, ya);
-}
-
-class Atom
-{
-	private:
-		int m_charge;
-		int m_rad;
-		int m_mass;
-		bool m_static;
-		Vector m_pos;
-		Vector m_velosity;
-		Vector m_accel;
-		Vector m_sumForce;
-		Vector m_elS;
-		sf::Color color;
-	public:
-		Atom(int charge = 1, int rad = 50, int mass = 1, bool staticP = 0, Vector vec = setVector(xPole/2 - rad, yPole/2 - rad)) : m_charge{charge}, m_rad{rad}, m_mass{mass}, m_static{staticP}, m_pos{vec}
-		{
-			//m_pos = setVector(960, 540);
-			m_pos.setVector(xPole/2 - m_rad, yPole/2 - m_rad);
-		}
-		friend class Continuum;
-		void chooseColour(double par)
-		{
-
-		}
-		void displayAtom(Atom ex)
-		{
-
-			sf::CircleShape atom(m_rad);
-		}
-		
-
-		
-
-	
-};
 class Example
 {
 	private:
@@ -133,8 +174,8 @@ class Example
 		sf::Color m_color();
 		int sx = 0;    
         	int sy = 0;
-	        int x = rand()%10; 
-        	int y = rand()%10;
+	        int x = rand()%30; 
+        	int y = rand()%30;
         	int setX = rand()%(xPole - rad * 2);
         	int setY = rand()%(yPole - rad * 2);
 		sf::CircleShape shape;
@@ -181,6 +222,9 @@ int main()
 	Example ex;
 	Example ex1;
 	Example ex2;
+	Example ex3;
+
+
 
 
 //	sf::RenderWindow window(sf::VideoMode(xPole, yPole), "std::string *Freudistic_name");
@@ -196,6 +240,7 @@ int main()
 		ex.displayEx();
 		ex1.displayEx();
 		ex2.displayEx();
+		ex3.displayEx();
 		window.display();
 		con.wait60();
 	}
