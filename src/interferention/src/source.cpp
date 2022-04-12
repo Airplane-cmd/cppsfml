097633337bddfdd320cb0b1506bb0e3d5d283f23
Airplane-cmd <simonpatrikoff1@outlook.com>
2022-04-12 12:56:07 +1000

    distancies

--- src/interferention/src/source.cpp
+++ src/interferention/src/source.cpp
@@ -13,6 +13,7 @@ class Oscilator
 		sf::Vector3f m_Pos;
 		double m_Lambda;
 		double m_Ez;
+		int m_ID;
 		std::chrono::system_clock::time_point m_startT;
 
 	public:
@@ -23,6 +24,10 @@ class Oscilator
 	double setValue(double r, double t)
 	{
 		return (m_Ez*cos(2*M_PI / m_Lambda*(t*C - r)));
+	}
+	sf::Vector3f getPos()
+	{
+		return m_Pos;
 	}
 	
 
@@ -38,39 +43,45 @@ class Screen
 		sf::Vector3f m_PSx;
 		sf::Vector3f m_PSy;
 		sf::Vector3f m_Pos;
-		std::vector<Oscilator*> m_Oscilator;
+		std::vector<Oscilator*> m_Oscilator;//public static
 		std::vector<std::vector<std::vector<double>>> m_Distance;
 		std::vector<std::vector<double>> m_Values;
 	public:
 		Screen(double X = 1920, double Y = 1080):m_X{X}, m_Y{Y}
 		{
-			std::vector<std::vector<std::vector<double>>> dist(*(&m_Oscilator + 1) - m_Oscilator);
+//			std::vector<std::vector<std::vector<double>>> dist(sizeof(m_Oscilator)/sizeof(m_Oscilator[0][0]), std::vector<std::vector<double>>(m_X, std::vector<double>(m_Y)));
 			std::vector<std::vector<double>> values(m_X, std::vector<double>(m_Y)); 
 			m_Values = values;
 		}
-		void addOsc(Oscilator osc)
+		std::vector<std::vector<double>> countDistances(Oscilator& osc)
 		{
-			m_Oscilator.push_back(&osc);
-		}
-		void countDistances()
-		{
+			std::vector<std::vector<double>>dist(m_X, std::vector<double>(m_Y));
 			for(int i = 0; i < m_X; ++i)
 			{
 				for(int j = 0; j < m_Y; ++j)
 				{
-					sf::Vector3f L;
-					 
+					sf::Vector3f L = osc.getPos() + m_Pos + static_cast<float>(i) * m_PSx + static_cast<float>(j) * m_PSy;
+					dist[i][j] = sqrt(pow(L.x, 2)+pow(L.y, 2)+pow(L.z, 2));
 
 				}
 			}
+			return dist;
 		}
+		void addOsc(Oscilator& osc)
+                {
+                        m_Oscilator.push_back(&osc);
+//			this->countDistances(osc);
+			m_Distance.push_back(this->countDistances(osc));
+                        
+                }
+
 		void setValues()
 		{
 			for(int i = 0; i < m_X; ++i)
 			{
 				for(int j = 0; j < m_Y; ++j)
 				{
-
+					 	
 				}
 			}
 		}
