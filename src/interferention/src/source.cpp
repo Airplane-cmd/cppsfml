f1d2b77a2a618e7f7b22a145b6910c431197fd39
Airplane-cmd <simonpatrikoff1@outlook.com>
2022-04-13 19:13:15 +1000

    cunt

--- src/interferention/src/source.cpp
+++ src/interferention/src/source.cpp
@@ -23,7 +23,7 @@ class Oscilator
 	}
 	double setValue(double r, double t)
 	{
-		return (m_Ez*cos(2*M_PI / m_Lambda*(t*C - r)));
+		return (pow(m_Ez*cos(2*M_PI / m_Lambda*(t*C - r)), 2));
 	}
 	sf::Vector3f getPos()
 	{
@@ -52,6 +52,11 @@ class Screen
 //			std::vector<std::vector<std::vector<double>>> dist(sizeof(m_Oscilator)/sizeof(m_Oscilator[0][0]), std::vector<std::vector<double>>(m_X, std::vector<double>(m_Y)));
 			std::vector<std::vector<double>> values(m_X, std::vector<double>(m_Y)); 
 			m_Values = values;
+			m_Pos = sf::Vector3f(1, 0, 0);
+			m_PSx = sf::Vector3f(0, 1, 0);
+			m_PSy = sf::Vector3f(0, 0, -1);
+			m_PS = 1.79e-4;
+
 		}
 		std::vector<std::vector<double>> countDistances(Oscilator& osc)
 		{
@@ -60,7 +65,7 @@ class Screen
 			{
 				for(int j = 0; j < m_Y; ++j)
 				{
-					sf::Vector3f L = osc.getPos() + m_Pos + static_cast<float>(i) * m_PSx + static_cast<float>(j) * m_PSy;
+					sf::Vector3f L = m_Pos - osc.getPos() + static_cast<float>(i) * m_PSx + static_cast<float>(j) * m_PSy;
 					dist[i][j] = sqrt(pow(L.x, 2)+pow(L.y, 2)+pow(L.z, 2));
 
 				}
@@ -75,31 +80,40 @@ class Screen
                         
                 }
 
-		void setValues()
+		void setValues(double t)
 		{
 			for(int i = 0; i < m_X; ++i)
 			{
 				for(int j = 0; j < m_Y; ++j)
 				{
-					 	
+					for(int k = 0; k < sizeof(m_Oscilator)/sizeof(m_Oscilator[0][0]); ++k)
+					{
+						m_Values[i][j] += m_Oscilator[k]->setValue(m_Distance[k][i][j], t);
+						if(m_Values[i][j] > m_Max)
+							m_Max = m_Values[i][j];
+						else if(m_Values[i][j] < m_Min)
+							m_Min = m_Values[i][j];
+					}
 				}
 			}
 		}
-		void setColor(std::vector<std::vector<sf::RectangleShape>>&pix)                      
+//		void setColor(std::vector<std::vector<sf::RectangleShape>>&pix)
+		void setColor(sf::RectangleShape pix, int i, int j)		
                 {       
                         double R, G, B;
                         G = 0;
-			for(int i = 0; i < m_X; ++i)
-                        {       
+			R = 0;
+//			for(int i = 0; i < m_X; ++i)
+//                      {       
 //                        	R = 255 - 255/sqrt(pow(m_Y,2)+pow(m_X, 2))*sqrt(pow(i, 2)+pow(j, 2)) ;
-                        	for(int j = 0; j < m_Y; ++j)
-                                {       
-                                        B = 255/sqrt(pow(m_Y,2)+pow(m_X, 2))*sqrt(pow(i, 2)+pow(j, 2));
-					R = 255 - 255/sqrt(pow(m_Y,2)+pow(m_X, 2))*sqrt(pow(i, 2)+pow(j, 2)) ;
-
-					pix[i][j].setFillColor(sf::Color(R, G, B));
-                                }       
-			}       
+//                        	for(int j = 0; j < m_Y; ++j)
+//                              {       
+//                                      B = 255/sqrt(pow(m_Y,2)+pow(m_X, 2))*sqrt(pow(i, 2)+pow(j, 2));
+//					R = 255 - 255/sqrt(pow(m_Y,2)+pow(m_X, 2))*sqrt(pow(i, 2)+pow(j, 2)) ;
+					B = 255 * (m_Values[i][j] - m_Min) /(m_Max - m_Min);	
+					pix.setFillColor(sf::Color(R, G, B));
+//                                }       
+//			}       
                 }
 		void render()
 		{
@@ -110,6 +124,9 @@ class Screen
 			std::vector<std::vector<sf::RectangleShape>>pixel(m_X, std::vector<sf::RectangleShape>(m_Y));
 			sf::RenderWindow win(sf::VideoMode(m_X, m_Y), s);
 			sf::RectangleShape ex(sf::Vector2f(1 ,1 ));
+			std::chrono::system_clock::time_point T;
+			std::chrono::system_clock::time_point GNS = std::chrono::system_clock::now();
+			std::chrono::duration<double, std::milli> t;
 			for(int i = 0; i < m_X; ++i)
 			{
 				for(int j = 0; j < m_Y; ++j)
@@ -120,30 +137,48 @@ class Screen
 			}
 			while(win.isOpen())
 			{
+//				T = std::chrono::system_clock::now();
+//				t = T - GNS;
 				sf::Event ev;
 				while(win.pollEvent(ev))
 				{
 					if(ev.type == sf::Event::Closed)
 						win.close();
 				}
-				this->setColor(pixel);
+				
+//				this->setColor(pixel);
+				T = std::chrono::system_clock::now();      
+                                t = T - GNS;
+
+				this->setValues(t.count()/1000);
 				for(int i = 0; i < m_X; ++i)
 				{
 					for(int j = 0; j < m_Y; ++j)
 					{
+						this->setColor(pixel[i][j], i, j);
 						win.draw(pixel[i][j]);
 					}
 				}
 				win.display();
 				win.clear();
 			}
+			std::cout<<"You are fucking cunt, suck my dick"<<std::endl;
 		}
+//		void Prfmnc(double t)
+//		{       
+//        		this->countDistancies();
+//			this->setValues(t);
+//			this->setColor();
+//			this->render();
+//			}
 };
 int main()
 {
 	double timeK = 1.0;
 	std::chrono::system_clock::time_point GNS = std::chrono::system_clock::now();
 	Screen src;
+	Oscilator osc;
+	src.addOsc(osc);
 	src.render();
 	return 0;
 }
